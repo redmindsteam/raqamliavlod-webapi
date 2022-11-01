9e6ea9a0d84f9891f8e8a4b7f3b0fd183bde8bec
Khushboqof <xushboqov.elbek@mail.ru>
2022-11-01 18:55:14 +0500

    Created Email Service Interface in Services from Service layer

---
+++ src/RaqamliAvlod.Infrastructure.Service/Interfaces/Common/IEmailService.cs
@@ -0,0 +1,14 @@
+﻿using RaqamliAvlod.Application.ViewModels.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaqamliAvlod.Infrastructure.Service.Interfaces.Common
+{
+    public interface IEmailService
+    {
+        public Task SendEmailAsync(EmailMessage message);
+    }
+}
