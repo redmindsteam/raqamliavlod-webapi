32ed4febbe768ba7880d35b603a0957fe5966f2e
Khushboqof <xushboqov.elbek@mail.ru>
2022-11-01 18:55:40 +0500

    Created Email Service in Services from Service layer

---
+++ src/RaqamliAvlod.Infrastructure.Service/Services/Common/EmailService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using RaqamliAvlod.Application.ViewModels.Common;
+using RaqamliAvlod.Infrastructure.Service.Interfaces.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaqamliAvlod.Infrastructure.Service.Services.Common
+{
+    public class EmailService : IEmailService
+    {
+        private readonly IConfigurationSection _config;
+
+        public EmailService(IConfigurationSection config)
+        {
+            _config = config;
+        }
+
+        public Task SendEmailAsync(EmailMessage message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
