17c597d03355990fa2b1a439a35aa2223a25611c
SobirjonovSodiqjonovich <otkirbeksobirjonov@gmail.com>
2022-11-13 23:07:19 +0500

    created rabbitmq consumers and producers

--- src/RaqamliAvlod.Infrastructure.Core/Interfaces/Shared/ICheckerProducer.cs
+++ src/RaqamliAvlod.Infrastructure.Core/Interfaces/Shared/ICheckerProducer.cs
@@ -1,6 +1,9 @@
-﻿namespace RaqamliAvlod.Infrastructure.Core.Interfaces.Shared
+﻿using RaqamliAvlod.Infrastructure.Core.Models;
+
+namespace RaqamliAvlod.Infrastructure.Core.Interfaces.Shared
 {
     public interface ICheckerProducer
     {
+        public void Send(CheckerSubmissionRequest request);
     }
 }
--- src/RaqamliAvlod.Infrastructure.Core/RabbitMq/RabbitMqCheckerProducer.cs
+++ src/RaqamliAvlod.Infrastructure.Core/RabbitMq/RabbitMqCheckerProducer.cs
@@ -1,6 +1,40 @@
-﻿namespace RaqamliAvlod.Infrastructure.Core.RabbitMQ
+﻿using Microsoft.Extensions.Configuration;
+using RabbitMQ.Client;
+using RaqamliAvlod.Infrastructure.Core.Interfaces.Shared;
+
+#pragma warning disable
+namespace RaqamliAvlod.Infrastructure.Core.RabbitMQ
 {
-    public class RabbitMqCheckerProducer
+    public class RabbitMqCheckerProducer : ICheckerProducer
     {
+        private readonly ConnectionFactory _factory;
+        private readonly IConnection _conn;
+        private readonly IModel _channel;
+        private readonly string _queueName;
+
+        public RabbitMqCheckerProducer(IConfiguration configuration)
+        {
+            var subconfiguration = configuration.GetSection("RabbitMq");
+            var uri = subconfiguration.GetSection("Uri").Value;
+            if (String.IsNullOrEmpty(uri) is false)
+                _factory = new ConnectionFactory() { Uri = new Uri(uri) };
+            else _factory = new ConnectionFactory()
+            {
+                HostName = subconfiguration.GetSection("Host").Value,
+                Port = int.Parse(subconfiguration.GetSection("Port").Value),
+                UserName = subconfiguration.GetSection("Username").Value,
+                Password = subconfiguration.GetSection("Password").Value,
+            };
+            _queueName = subconfiguration.GetSection("ProducerRabbitQueue").Value;
+
+            _conn = _factory.CreateConnection();
+            _channel = _conn.CreateModel();
+            _channel.QueueDeclare(queue: _queueName,
+                                    durable: true,
+                                    exclusive: false,
+                                    autoDelete: false,
+                                    arguments: null);
+            _channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+        }
     }
 }
