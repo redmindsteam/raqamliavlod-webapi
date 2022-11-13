273a2f554d1b26f9390b313bcc46a3c8698a7310
SobirjonovSodiqjonovich <otkirbeksobirjonov@gmail.com>
2022-11-14 01:02:43 +0500

    Updated Core Layer

--- src/RaqamliAvlod.Infrastructure.Core/Models/CheckerSubmissionRequest.cs
+++ src/RaqamliAvlod.Infrastructure.Core/Models/CheckerSubmissionRequest.cs
@@ -1,19 +1,7 @@
 ﻿namespace RaqamliAvlod.Infrastructure.Core.Models
 {
-    public class CheckerSubmissionRequest : CheckerSubmissionOptions
+    public class CheckerSubmissionRequest : CheckerSubmissionDetails
     {
-        public string ProblemKey { get; set; } = String.Empty;
-
-        public string Language { get; set; } = String.Empty;
-
-        public byte[] Code { get; set; } = null!;
-
-        public string CodeFileName { get; set; } = String.Empty;
-
-        public ushort TimeLimit { get; set; }
-
-        public uint MemoryLimit { get; set; }
-
         public string IdentityKey { get; set; } = String.Empty;
 
         public string Password { get; set; } = String.Empty;
